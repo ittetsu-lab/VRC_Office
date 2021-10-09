e9e2080d3fa3a0f187e28a1e077ea9da68db4b1d
DESKTOP-JF0JBO3\issei <issei.k.rd5@gmail.com>
2021-10-09 23:37:24 +0900

    Add Earth

--- Assets/Snail/Marker/Marker3.0/AnimationGenerator/SnailMarker3AnimationCreator.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[RequireComponent(typeof(TrailRenderer))]
-public class SnailMarker3AnimationCreator : MonoBehaviour {
-    
-}
